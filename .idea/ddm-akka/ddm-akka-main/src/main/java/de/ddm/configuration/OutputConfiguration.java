5360c592b2680ed171b1b16370fd6713f816729c
Ahmed Mustafa <70472270+Gezatra@users.noreply.github.com>
2025-12-06 16:52:15 +0100

    Delete .idea directory

--- .idea/ddm-akka/ddm-akka-main/src/main/java/de/ddm/configuration/OutputConfiguration.java
+++
@@ -1,9 +0,0 @@
-package de.ddm.configuration;
-
-import lombok.Data;
-
-@Data
-public class OutputConfiguration {
-
-	private final String outputFileName = "results.txt";
-}
--- .idea/ddm-akka/ddm-akka-main/src/main/java/de/ddm/serialization/AkkaSerializable.java
+++
@@ -1,9 +0,0 @@
-package de.ddm.serialization;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-
-import java.io.Serializable;
-
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public interface AkkaSerializable extends Serializable {
-}
